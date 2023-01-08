bf47caee286a32ec87bc6e769ca66325ed54141e
vincentinttsh <vincentinttsh@gmail.com>
2023-01-08 19:36:13 +0800

    merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
-module.exports = {
-  content: ["./template/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    content: ["./template/**/*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('tw-elements/dist/plugin')
+    ],
+  }
+} else {
+  module.exports = {
+    content: ["./template/**/*.html", './node_modules/tw-elements/dist/js/**/*.js'],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('tw-elements/dist/plugin')
+    ],
+  }
 }
