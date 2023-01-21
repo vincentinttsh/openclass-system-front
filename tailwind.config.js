1c0a33a1edbc8085d2335a66e44e7d3f159a4305
vincentinttsh <vincentinttsh@gmail.com>
2023-01-21 20:17:22 +0800

    do lots of stuff
    1. create openclass page
    2. toast component
    3. prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,11 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    content: ["./template/**/*.html"],
-    theme: {
-      screens: {
-        lg: '992px',
-      },
+module.exports = {
+  content: ["./template/**/*.html", './node_modules/tw-elements/dist/js/index.min.js'],
+  theme: {
+    screens: {
+      lg: '992px',
     },
-    plugins: [
-      require('tw-elements/dist/plugin')
-    ],
-  }
-} else {
-  module.exports = {
-    content: ["./template/**/*.html", './node_modules/tw-elements/dist/js/**/*.js'],
-    theme: {
-      screens: {
-        lg: '992px',
-      },
-    },
-    plugins: [
-      require('tw-elements/dist/plugin')
-    ],
-  }
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 }
